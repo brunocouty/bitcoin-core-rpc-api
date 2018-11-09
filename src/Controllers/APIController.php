9fa4cc1328bb8dc8074f68918673ab91be47476a
Bruno <brunocouty@gmail.com>
2018-11-09 05:12:51 -0200

    #0 - Em andamento;

--- src/Controllers/APIController.php
+++ src/Controllers/APIController.php
@@ -2,30 +2,98 @@
 
 namespace BrunoCouty\BitcoinCoreRPCAPI\Controllers;
 
-
 use BrunoCouty\BitcoinCoreRPCAPI\Services\Bitcoin;
 
 class APIController
 {
-    public function auth()
+    /**
+     * Input access credentials.
+     * @return Bitcoin
+     */
+    private function auth()
     {
         $conn = new Bitcoin(
             config('bitcoin-api.rpc_user'),
             config('bitcoin-api.rpc_password')
         );
-        if ($conn === false) {
-            return "Error on connect.";
-        }
         return $conn;
     }
 
+    /**
+     * Test the capabilities to run commands on bitcoin server.
+     * @return \Illuminate\Contracts\Routing\ResponseFactory|\Symfony\Component\HttpFoundation\Response
+     */
     public function test()
     {
-        return $this->auth()->getnewaddress();
+        if ($this->auth()->getwalletinfo() == false) {
+            return response([
+                'data' => [
+                    'message' => "The server returns 'false'. Verify the service status and access data."
+                ]
+            ], 412);
+        }
     }
 
-    public function getAddress()
+    /**
+     * Test if the method return a valid value.
+     * @param $response
+     * @return bool|\Illuminate\Contracts\Routing\ResponseFactory|\Symfony\Component\HttpFoundation\Response
+     */
+    private function test_response($response)
     {
+        if ($response == false) {
+            return response([
+                'data' => [
+                    'message' => "The server returns 'false'. Verify the service status and access data."
+                ]
+            ], 412);
+        }
+        return true;
+    }
 
+    /**
+     * Return a valid wallet address from Bitcoin server.
+     * @return bool|\Illuminate\Contracts\Routing\ResponseFactory|\Symfony\Component\HttpFoundation\Response
+     */
+    public function getNewAddress()
+    {
+        $address = $this->auth()->getnewaddress();
+        $test = $this->test_response($address);
+        if ($test->getStatusCode()) {
+            return $test;
+        }
+        return $address;
+    }
+
+    /**
+     * Return the current balance from Bitcoin server.
+     * @return bool|\Illuminate\Contracts\Routing\ResponseFactory|\Symfony\Component\HttpFoundation\Response
+     */
+    public function getCurrentBalance()
+    {
+        $balance = $this->auth()->getbalance();
+        $test = $this->test_response($balance);
+        if ($test->getStatusCode() == 412) {
+            return $test;
+        }
+        return $balance;
+    }
+
+    /**
+     * Send bitcoins to another address.
+     * @param $wallet
+     * @param $btc_amount
+     * @return bool|\Illuminate\Contracts\Routing\ResponseFactory|\Symfony\Component\HttpFoundation\Response
+     */
+    public function sendBitcoins($wallet, $btc_amount)
+    {
+        $txID = $this->auth()->sendtoaddress($wallet, $btc_amount);
+        $test = $this->test_response($txID);
+        if ($test->getStatusCode() != 201) {
+            return $test;
+        }
+        return response([
+
+        ], 201);
     }
 }
